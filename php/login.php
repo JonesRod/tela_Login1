f214b80218cfb31f77e0e50322723c648ad3201c
JonesRod <batatajonesrodrigues@gmail.com>
2023-08-04 16:15:13 -0400

    melhorando códigos

--- php/login.php
+++ php/login.php
@@ -1,7 +1,7 @@
 <?php
 
-include('lib/conexao.php');
-include('lib/protect.php');
+include('../lib/php/conexao.php');
+include('../lib/php/protect.php');
 
 if (isset($_POST['email']) || isset($_POST['senha'])) {
 
@@ -26,7 +26,7 @@ if (isset($_POST['email']) || isset($_POST['senha'])) {
 
                     $_SESSION['usuario'] = $usuario['id'];
                     
-                    header("Location: index.php");
+                    header("Location: php/index.php");
             }else{
                 echo "Senha inválida";
             }
@@ -43,29 +43,29 @@ if (isset($_POST['email']) || isset($_POST['senha'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="../css/login.css">
     <title>Entrar</title>
 </head>
 <body>
-    <div class="conteiner">
-        <h1>Entrar</h1>
+    <h1 id="titulo">Entrar</h1>
+    <main class="conteiner">
         <div id="login">
             <form action="" method="POST">
-            <p>
-                <label for="">E-mail</label>
-                <input type="text" name="email">
-            </p>
-            <p>
-                <label for="">Senha</label>
-                <input type="password" name="senha">
-            </p>
-            <p>
-                <a style="margin-right:40px;" href="cadastro_usuario.php">Quero me Cadastrar.</a> 
-                <a style="margin-right:40px;" href="Recupera_Senha.php">Esqueci minha Senha!</a> 
-            </p>
-            <button type="submit">Entrar</button>
-        </form>
-    </div>
-    </div>
+                <p>
+                    <label for="">E-mail</label>
+                    <input type="text" name="email">
+                </p>
+                <p>
+                    <label for="">Senha</label>
+                    <input type="password" name="senha">
+                </p>
+                <p>
+                    <a style="margin-right:40px;" href="php/cadastro_usuario.php">Quero me Cadastrar.</a> 
+                    <a style="margin-right:40px;" href="php/Recupera_Senha.php">Esqueci minha Senha!</a> 
+                </p>
+                <button type="submit">Entrar</button>
+            </form>
+        </div>
+    </main>
 </body>
 </html>
