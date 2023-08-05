5ea64ac3d0aac79de3156b12614ff2b0bfde0a65
JonesRod <batatajonesrodrigues@gmail.com>
2023-08-05 13:53:53 -0400

    organizando index

--- index.php
+++ index.php
@@ -1,15 +1,37 @@
+<?php
+include('lib/conexao.php');
+
+if(!isset($_SESSION))
+    session_start();
+
+if(!isset($_SESSION['usuario'])){
+    die("Você não pode acessar esta página porque não está logado.<p><a href=\"php/login.php\">Entrar</a></p>");
+}
+if(isset($_SESSION['email'])){
+
+    $email = $_SESSION['email'];
+    $senha = password_hash($_SESSION['senha'], PASSWORD_DEFAULT);
+    $mysqli->query("INSERT INTO senha (email, senha) VALUES('$email','$senha')");
+
+}
+
+$id = $_SESSION['usuario'];
+$sql_query = $mysqli->query("SELECT * FROM usuarios WHERE id = '$id'") or die($mysqli->error);
+$usuario = $sql_query->fetch_assoc();
+?>
+
 <!DOCTYPE html>
 <html lang="pt">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="import" href="php/login.php">
 </head>
 <body>
-   <h1>oiiiiiiiiiiiiiiiiiiiii</h1>
-    <!--//Essa tela de código serve para inciar o site com index e redirecionar para tela de login.
-    <?php header("Location: php/login.php"); ?>
-    
+   <p>Bem Vindo, <?php echo $usuario['nome']; ?></p>
+    <p>
+        <a href="editar_dados_usuario.php">Editar dados do Usuario</a><br>
+        <a href="logout.php">Sair</a>
+    </p>
 </body>
 </html>
