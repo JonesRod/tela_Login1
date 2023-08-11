ba299e07b3d5a579e4705e0a7dac521230ffdd18
JonesRod <batatajonesrodrigues@gmail.com>
2023-08-11 07:55:20 -0400

    começando 40 ribas

--- index.php
+++ index.php
@@ -53,12 +53,13 @@ if (isset($_POST['email']) || isset($_POST['senha'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="lib/css/login.css">
+    <link rel="stylesheet" href="lib/css/index.css">
     <title>Entrar</title>
 </head>
 <body>
     <main class="conteiner">   
-        <form action="" method="POST">
+        <form id ="login" action="" method="POST">
+            <div id="img"></div>
             <h1 id="titulo">Entrar</h1>
             <span>
                 <?php 
